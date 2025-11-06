ed704dca6f936b00549beb74dfac4b617d1a504e
GiulliaM <giulliameneses@gmail.com>
2025-11-05 21:41:47 -0300

    algumas mudança no estilo

--- frontend/src/constantes/cores.ts
+++ frontend/src/constantes/cores.ts
@@ -7,6 +7,8 @@ interface PaletaCores{
     fundo: string;
     preto: string;
     branco: string;
+    azulEscuro: string;
+    azulClaro: string;
 
 }
 
@@ -19,5 +21,6 @@ export const cores: PaletaCores = {
     fundo: '#c7f9cc',
     preto: '#000000',
     branco: '#ffffff',
-
+    azulEscuro: '#0B3D91',
+    azulClaro: '#0097A7',
 };
