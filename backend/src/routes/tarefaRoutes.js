652c23fdf39d31d63a51689934fe18dfeceed8eb
Maria Eduarda Rodrigues <msriarodriggues42@gmail.com>
2025-11-12 13:52:29 -0300

    correcao atrefa routes

--- backend/src/routes/tarefaRoutes.js
+++ backend/src/routes/tarefaRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { postTarefa, getTarefas, deleteTarefa, patchTarefa } from "../controllers/tarefaController.js";
+import { createTarefa, getTarefas, deleteTarefa, updateTarefa } from "../controllers/tarefaController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 const router = express.Router();
-router.post("/", authMiddleware, postTarefa);
+router.post("/", authMiddleware, createTarefa);
 router.get("/", authMiddleware, getTarefas);
-router.patch("/:id", authMiddleware, patchTarefa);
+router.patch("/:id", authMiddleware, updateTarefa);
 router.delete("/:id", authMiddleware, deleteTarefa);
 export default router;
